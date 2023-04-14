e8776dbb39907b4830fa4f20caa92cb209ec0d30
Jejoon Ryu <rjejoon@gmail.com>
2023-04-14 00:45:09 -0600

    Hide scroll bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Normalize } from 'styled-normalize';
-import { createGlobalStyle } from 'styled-components';
-
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Normalize } from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  :root {
+  html {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    max-height: 100%;
     overflow: hidden;
   }
 
@@ -15,7 +20,7 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
